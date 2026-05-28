b31f555a4650ba52411a79b521388913f06de394
Ansh <garvgupta2984@gmail.com>
2026-05-28 22:21:10 +0530

    refactor(cpp): update shared headers and formatting config

---
+++ cpp/imports/common.h
@@ -0,0 +1,26 @@
+// Common macros for competitive programming
+// Use with #include <bits/stdc++.h> and using namespace std;
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define rrep(i, a, b) for(int i = a; i >= b; i--)
+#define endl '\n'
+
+// Debug macro
+#ifdef DEBUG
+#define debug(x) cout << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
